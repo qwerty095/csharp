d9403572cb16485ab64896baa4b07dc348cd15d2
qwerty095 <v261087@mail.ru>
2023-04-11 08:57:02 +0700

    variables

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            bool catOrDog;
+            byte veryShortHexNum;
+            string animalName;
+            char firstLetterAnimalName;
+            short countCat;
+            int countDog;
+            uint numberOfPaws;
+            long lengthOfWool;
+            float tonsOfFoodPerMonth;
+            double tonsOfFoodPerYear;
         }
     }
 }
